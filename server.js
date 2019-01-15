aaabda35aa44c5b9fcb3287c70f573510cb3c4f7
mitchellewulonu <ewulonu.mitchell@yahoo.ie>
2019-01-15 23:23:37 +0000

    aeroassists

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get("/api/search", function(req, res) {
     if (err) {
       handleError(res, err.message, "Failed to get information.");
     } else {
-      res.json(docs);
+      res.status(200).json(docs);
     }
   });
 });
