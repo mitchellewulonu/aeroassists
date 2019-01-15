1dc60064a63ea964e912b95c522e8f5c23788fc2
mitchellewulonu <ewulonu.mitchell@yahoo.ie>
2019-01-15 22:32:09 +0000

    aeroassists

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api', search);
 var server = app.listen(process.env.PORT || 8080, function(){
 	
 	var port = server.address().port;
-    console.log("App now running on port" + port);
+    console.log("App now running on port " + port);
 });
 
 
