b146f31dd132de52ddcc0dcb0426ba037b2f86ff
mitchellewulonu <ewulonu.mitchell@yahoo.ie>
2019-01-17 15:18:23 +0000

    aeroassists

--- src/app/component/results/results.component.ts
+++ src/app/component/results/results.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {SearchService} from '../../services/search/search.service'
 import  {search} from '../../../search';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-results',
