5068549aa4659d5d36840810f3163ff2821a525e
mitchellewulonu <ewulonu.mitchell@yahoo.ie>
2019-04-17 01:06:11 +0100

    used the mongodb env variable

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(express.static(distDir));
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect("mongodb://admin:89Alexandra@ds251894.mlab.com:51894/aeroassists" || "mongodb://localhost:27017/test", function (err, client) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://admin:89Alexandra@ds251894.mlab.com:51894/aeroassists", function (err, client) {
   if (err) {
     console.log(err);
     process.exit(1);
